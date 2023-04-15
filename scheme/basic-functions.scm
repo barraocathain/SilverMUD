f488a4ac7c641d7cf4e47a015ac116720eb69df2
Barry Kane <barra@ocathain.ie>
2023-04-15 23:46:06 +0100

    Add a comment explaining what shout does.

--- scheme/basic-functions.scm
+++ scheme/basic-functions.scm
@@ -3,5 +3,6 @@
 							   (create-path from-path-description from-area to-area arealist)
 							   (create-path to-path-description to-area from-area arealist)))
 
-(define (shout message outputqueue)
-  (message-everyone "SERVER" message outputqueue))
+;; Send a quick message to everyone in the game as the server:
+(define (shout message)
+  (message-everyone "SERVER" message output-queue))
