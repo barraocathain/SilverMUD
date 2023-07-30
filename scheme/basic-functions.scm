2059aa0b21cba64006f83a5bf544011a3b3e8394
Barry Kane <barra@ocathain.ie>
2023-07-30 22:18:23 +0100

    Starting again, see old branch for old codebase.

--- scheme/basic-functions.scm
+++
@@ -1,9 +0,0 @@
-(define (create-two-way-path from-path-description to-path-description from-area to-area arealist)
-							 (begin
-							   (create-path from-path-description from-area to-area arealist)
-							   (create-path to-path-description to-area from-area arealist)))
-
-;; Send a quick message to everyone in the game as the server:
-(define (shout message)
-  "Send a quick message to everyone in the game as the server."
-  (message-everyone "SERVER" message output-queue))
